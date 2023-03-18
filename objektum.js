53b7dd926ede581320f919c2a4c652f810677fde
marybogyo <bratis.marianna@gmail.com>
2023-03-18 09:33:38 +0100

    mod

--- objektum.js
+++ objektum.js
@@ -1,12 +1,20 @@
 window.addEventListener("load", init);
 const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
 function init(){
-const ELEM = document.getElementsByClassName("gomb")
-
-szoveg = "";
-for (let index = 0; index < kutyaLista.length; index++) {
-    szoveg += `<button> $ "Kutya" </button>}`
-    szoveg += 
+//*const ELEM = document.querySelector("article")
+//*ELEM.innerHTML= "<button>Kutya</button>" 
+//*const gomb = document.querySelectorAll("article buttn")[0]q
+const ARTICLE=document.querySelectorAll("article")
+/**Írjuk ki a divekbe és azon belül p tagbe a kutyák fajtáját */
+osszeallit()
+//itt helyezzük bele az articlebe
 }
+
+function osszeallit(){
+    let txt=""
+    for (let index = 0; index < kutyaLista.length; index++) {
+             txt+=`<div><p>nev</p></div>`   
+    }
+    console.log(txt)
 }
 
