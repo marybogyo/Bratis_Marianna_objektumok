aa02b5cd8cb5370eef6ec70e66d9cc4849063fde
marybogyo <bratis.marianna@gmail.com>
2023-03-18 08:51:33 +0100

    vége

--- objektum.js
+++ objektum.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", init);
 const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
-function(){
+function init(){
 const ELEM = document.getElementsByClassName("gomb")
 
 szoveg = "";
 for (let index = 0; index < kutyaLista.length; index++) {
-    szoveg += `<button> $ ""}`
-    szoveg
+    szoveg += `<button> $ "Kutya" </button>}`
+    szoveg += 
 }
 }
 
