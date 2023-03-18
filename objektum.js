93ddc74e4ff0259dfacfbd5245eb6573eea080bb
marybogyo <bratis.marianna@gmail.com>
2023-03-18 11:12:58 +0100

    css

--- objektum.js
+++ objektum.js
@@ -1,20 +1,34 @@
 window.addEventListener("load", init);
-const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
+const kutyaLista=[ {nev: "Dézi", kor: 12, fajta: "foxterrier"},
+{nev: "Dézi", kor: 10,fajta: "kuvasz"},
+{nev: "Dézi", kor: 9,fajta: "puli"},
+{nev: "Dézi", kor: 8,fajta: "agár"},
+{nev: "Dézi", kor: 7,fajta: "újfullandi"},
+{nev: "Dézi", kor: 6,fajta: "keverék"}
+]
 function init(){
 //*const ELEM = document.querySelector("article")
 //*ELEM.innerHTML= "<button>Kutya</button>" 
 //*const gomb = document.querySelectorAll("article buttn")[0]q
 const ARTICLE=document.querySelectorAll("article")
 /**Írjuk ki a divekbe és azon belül p tagbe a kutyák fajtáját */
-osszeallit()
+let txt=osszeallit()
 //itt helyezzük bele az articlebe
+ARTICLE[0].innerHTML=txt
 }
 
 function osszeallit(){
     let txt=""
     for (let index = 0; index < kutyaLista.length; index++) {
-             txt+=`<div><p>nev</p></div>`   
+             txt+=`<div>`
+             for (const key in kutyaLista[index]) {
+                txt+=`<p>${kutyaLista[index][key]}</p>`
+        
+             }
+           
+             txt+= `</div>`   
     }
     console.log(txt)
+    return txt
 }
 
