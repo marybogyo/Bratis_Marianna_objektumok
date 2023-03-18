a1f2da32a94f674085989e087fa78ee877d88303
marybogyo <bratis.marianna@gmail.com>
2023-03-18 08:46:21 +0100

    szoveg letrehozas

---
+++ objektum.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", init);
+const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
+function(){
+const ELEM = document.getElementsByClassName("gomb")
+
+szoveg = "";
+for (let index = 0; index < kutyaLista.length; index++) {
+    szoveg += `<button> $ ""}`
+    szoveg
+}
+}
+
