d86198fba6ec5304d45ad2c48b6f29ab4af56127
marybogyo <bratis.marianna@gmail.com>
2023-03-19 17:44:00 +0100

    űrlap

--- objektum.js
+++ objektum.js
@@ -16,10 +16,14 @@ function init() {
   let txt = osszeallit();
   //itt helyezzük bele az articlebe
   ARTICLE[0].innerHTML = txt;
+  
+  const ASIDE = document.querySelectorAll("aside");
+  ASIDE[0].appendChild(urlap());
 }
 const NEV = "Név: ";
 const KOR = "Kor: ";
 const FAJTA = "Fajta: ";
+
 function osszeallit() {
   let txt = "";
   for (let index = 0; index < kutyaLista.length; index++) {
@@ -50,3 +54,96 @@ function osszeallit() {
   console.log(txt);
   return txt;
 }
+
+function urlap() {
+    let form = document.createElement("form");
+  
+    let nameLabel = document.createElement("label");
+    nameLabel.textContent = "Kutya neve:";
+    form.appendChild(nameLabel);
+    
+  
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("class", "bgc");
+    nameInput.setAttribute("id", "inputNev");
+    nameInput.setAttribute("name", "nev");
+    form.appendChild(nameInput);
+  
+    let ageLabel = document.createElement("label");
+    ageLabel.textContent = "Kora:";
+    form.appendChild(ageLabel);
+  
+    let ageInput = document.createElement("input");
+    ageInput.setAttribute("type", "text");
+    ageInput.setAttribute("class", "bgc");
+    ageInput.setAttribute("id", "inputKor");
+    ageInput.setAttribute("name", "kor");
+    form.appendChild(ageInput);
+  
+    let breedLabel = document.createElement("label");
+    breedLabel.textContent = "Fajtája:";
+    form.appendChild(breedLabel);
+  
+    let breedInput = document.createElement("input");
+    breedInput.setAttribute("type", "text");
+    breedInput.setAttribute("class", "bgc");
+    breedInput.setAttribute("id", "inputFajta");
+    breedInput.setAttribute("name", "fajta");
+    form.appendChild(breedInput);
+  
+    let submitButton = document.createElement("input");
+    submitButton.setAttribute("type", "button");
+    submitButton.setAttribute("class", "felvet");
+    submitButton.setAttribute("value", "Felvétel");
+    submitButton.setAttribute("onClick", "osszeallitButton()");
+    
+    form.appendChild(submitButton);
+  
+    return form;
+  }
+
+  function osszeallitButton() {
+    let iNev = document.getElementById("inputNev").value;
+    let iKor = document.getElementById("inputKor").value;
+    let iFajta = document.getElementById("inputFajta").value;
+
+    if (iNev != "" && iKor != "" && iFajta != "")
+    {
+        let txt = "<div>";
+
+      let sorrend = 0;
+      let mi = "";
+      for (let index = 0; index < 3; index++) {
+        let megnevezes = "";
+        switch (sorrend) {
+          case 0:
+            megnevezes = NEV;
+            mi = iNev;
+            sorrend += 1;
+            break;
+          case 1:
+            megnevezes = KOR;
+            mi = iKor;
+            sorrend += 1;
+            break;
+          case 2:
+            megnevezes = FAJTA;
+            mi = iFajta;
+            break;
+          default:
+            break;
+        }
+        txt += `<p>${megnevezes + mi}</p>`;
+      }
+      txt += `<button onclick="this.parentNode.remove()">Gomb</button>`;
+      txt += `</div>`;
+    console.log(txt);
+    
+    document.querySelectorAll("article")[0].innerHTML += txt;
+
+    document.getElementById("inputNev").value = "";
+    document.getElementById("inputKor").value = "";
+    document.getElementById("inputFajta").value = "";
+    }    
+  }
